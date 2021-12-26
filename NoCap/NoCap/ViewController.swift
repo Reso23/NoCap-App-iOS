81360d4a191eecfdb26ef85e7e083176da74ee5b
Reso23 <84687573+Reso23@users.noreply.github.com>
2021-12-25 21:20:36 -0500

    Add files via upload

---
+++ NoCap/NoCap/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  NoCap
+//
+//  Created by Misael on 12/25/21.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        playSound()
+}
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "stop_the_cap", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
+    
+
